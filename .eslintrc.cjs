66a033ee059f857c31f2557e57531d6c5eb8e3fe
Yijing Chen <yijing@duolingo.com>
2020-08-12 15:47:03 -0400

    Set up eslint TS resolver.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,16 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error",
     "sort-imports": [
@@ -19,5 +29,10 @@ module.exports = {
         ignoreCase: true,
       },
     ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
   },
 };
