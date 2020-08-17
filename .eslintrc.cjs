048941860cd760c26e2c8175cb2bbcd37dc00385
Yijing Chen <yijing@duolingo.com>
2020-08-17 12:19:15 -0400

    Apply ts-specific no-unused-vars rule.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,8 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
     "sort-imports": [
       "error",
       {
