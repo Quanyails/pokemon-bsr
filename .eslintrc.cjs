a07a1dd880e2ed40ab1d27773a41b812e86b67f8
Yijing Chen <yijing@duolingo.com>
2020-08-17 12:13:53 -0400

    Configure ESLint.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,12 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
-  rules: {},
+  rules: {
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: true,
+      },
+    ],
+  },
 };
